7e3e6f60b2185419c35954bea5c9157d5f1f308d
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-04-26 12:48:41 +0300

    Add utils module tests

--- openprocurement/tender/core/tests/main.py
+++ openprocurement/tender/core/tests/main.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from openprocurement.tender.core.tests import tender
+from openprocurement.tender.core.tests import tender, utils
 
 
 def suite():
     suite = unittest.TestSuite()
+    suite.addTest(utils.suite())
     suite.addTest(tender.suite())
     return suite
 
