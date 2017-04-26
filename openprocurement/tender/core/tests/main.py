e99376550c9103193898dc14b4465381e426aa39
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-04-26 14:02:18 +0300

    Add belowthreshold tests

--- openprocurement/tender/core/tests/main.py
+++ openprocurement/tender/core/tests/main.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from openprocurement.tender.core.tests import tender, utils, models, auth
+from openprocurement.tender.core.tests import (
+    auth, auction, award, bid, cancellation,
+    chronograph, complaint, contract, document,
+    models, lot, tender, question, utils
+)
 
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(models.suite())
-    suite.addTest(utils.suite())
     suite.addTest(auth.suite())
+    suite.addTest(auction.suite())
+    suite.addTest(award.suite())
+    suite.addTest(bid.suite())
+    suite.addTest(cancellation.suite())
+    suite.addTest(chronograph.suite())
+    suite.addTest(complaint.suite())
+    suite.addTest(contract.suite())
+    suite.addTest(document.suite())
+    suite.addTest(models.suite())
+    suite.addTest(lot.suite())
     suite.addTest(tender.suite())
+    suite.addTest(question.suite())
+    suite.addTest(utils.suite())
     return suite
 
 
