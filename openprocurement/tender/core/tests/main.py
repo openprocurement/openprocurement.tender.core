ff1fa3765162e0a9ede9a694b4f68de9eb187fe3
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-04-26 13:05:01 +0300

    Add auth tests

--- openprocurement/tender/core/tests/main.py
+++ openprocurement/tender/core/tests/main.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from openprocurement.tender.core.tests import tender, utils, models
+from openprocurement.tender.core.tests import tender, utils, models, auth
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(models.suite())
     suite.addTest(utils.suite())
+    suite.addTest(auth.suite())
     suite.addTest(tender.suite())
     return suite
 
