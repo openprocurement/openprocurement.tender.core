eb77f0fd2e85485f60f18704ae23e8f4b69fc23c
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-04-27 16:28:52 +0300

    Add test data

--- openprocurement/tender/core/tests/tender.py
+++ openprocurement/tender/core/tests/tender.py
@@ -1,9 +1,92 @@
 # -*- coding: utf-8 -*-
 import os
+from datetime import timedelta
+
+from openprocurement.api.utils import get_now
+from openprocurement.api.constants import ROUTE_PREFIX, SANDBOX_MODE
 from openprocurement.tender.core.tests.base import BaseWebTest
 
 
+now = get_now()
+test_organization = {
+    "name": u"Державне управління справами",
+    "identifier": {
+        "scheme": u"UA-EDR",
+        "id": u"00037256",
+        "uri": u"http://www.dus.gov.ua/"
+    },
+    "address": {
+        "countryName": u"Україна",
+        "postalCode": u"01220",
+        "region": u"м. Київ",
+        "locality": u"м. Київ",
+        "streetAddress": u"вул. Банкова, 11, корпус 1"
+    },
+    "contactPoint": {
+        "name": u"Державне управління справами",
+        "telephone": u"0440000000"
+    }
+}
+test_procuringEntity = test_organization.copy()
+test_procuringEntity["kind"] = "general"
+test_tender_data = {
+    "title": u"футляри до державних нагород",
+    "procuringEntity": test_procuringEntity,
+    "value": {
+        "amount": 500,
+        "currency": u"UAH"
+    },
+    "minimalStep": {
+        "amount": 35,
+        "currency": u"UAH"
+    },
+    "items": [
+        {
+            "description": u"футляри до державних нагород",
+            "classification": {
+                "scheme": u"CPV",
+                "id": u"44617100-9",
+                "description": u"Cartons"
+            },
+            "additionalClassifications": [
+                {
+                    "scheme": u"ДКПП",
+                    "id": u"17.21.1",
+                    "description": u"папір і картон гофровані, паперова й картонна тара"
+                }
+            ],
+            "unit": {
+                "name": u"item",
+                "code": u"44617100-9"
+            },
+            "quantity": 5,
+            "deliveryDate": {
+                "startDate": (now + timedelta(days=2)).isoformat(),
+                "endDate": (now + timedelta(days=5)).isoformat()
+            },
+            "deliveryAddress": {
+                "countryName": u"Україна",
+                "postalCode": "79000",
+                "region": u"м. Київ",
+                "locality": u"м. Київ",
+                "streetAddress": u"вул. Банкова 1"
+            }
+        }
+    ],
+    "enquiryPeriod": {
+        "endDate": (now + timedelta(days=7)).isoformat()
+    },
+    "tenderPeriod": {
+        "endDate": (now + timedelta(days=14)).isoformat()
+    },
+    "procurementMethodType": "belowThreshold",
+}
+if SANDBOX_MODE:
+    test_tender_data['procurementMethodDetails'] = 'quick, accelerator=1440'
+
+
 class TenderResourceTest(BaseWebTest):
+    initial_auth = ('Basic', ('broker', ''))
     relative_to = os.path.dirname(__file__)
 
     def test_empty_listing(self):
@@ -66,4 +149,3 @@ class TenderResourceTest(BaseWebTest):
         self.assertIn('limit=10', response.json['next_page']['uri'])
         self.assertNotIn('descending=1', response.json['prev_page']['uri'])
         self.assertIn('limit=10', response.json['prev_page']['uri'])
-
